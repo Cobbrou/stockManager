77a4fc8a40678c0e72648edfe00672b00716f3a4
Cobbrou <tom.nrj@orange.fr>
2021-09-21 18:46:50 +0200

    Starting the base system

--- core/data.py
+++ core/data.py
@@ -1,11 +1,45 @@
 import sqlite3
 
-def create_DB():
-    conn = sqlite3.connect('data/data.db')
-    c = conn.cursor()
-    c.execute("""CREATE TABLE IF NOT EXISTS stock(
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        price REAL,
-        quantity INTEGER
-    )""")
+
+class DB:
+    def __init__(self, name):
+        self.name = "data.db"
+        self.conn = sqlite3.connect(name)
+        self.c = self.conn.cursor()
+
+    def init_table(self):
+        self.c.execute("""CREATE TABLE IF NOT EXISTS stock(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            price REAL,
+            quantity INTEGER
+        )""")
+
+    def add_item(self, name, price, quantity):
+        self.c.execute("""INSERT INTO stock(name, price, quantity)
+            VALUES(?, ?, ?)""", (name, price, quantity))
+        self.conn.commit()
+
+    def remove_item(self, name):
+        self.c.execute("""DELETE FROM stock WHERE name=?""", (name,))
+        self.conn.commit()
+
+    def create_item(self, name, price, quantity):
+        self.c.execute("""INSERT INTO stock(name, price, quantity)
+            VALUES(?, ?, ?)""", (name, price, quantity))
+        self.conn.commit()
+
+    def delete_item(self, name):
+        self.c.execute("""DELETE FROM stock WHERE name=?""", (name,))
+        self.conn.commit()
+
+    def get_item(self, name):
+        self.c.execute("""SELECT * FROM stock WHERE name=?""", (name,))
+        return self.c.fetchone()
+
+    def get_all(self):
+        self.c.execute("""SELECT * FROM stock""")
+        return self.c.fetchall()
+
+    def close(self):
+        self.conn.close()
--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os, sys, socket
 import sqlite3
 
 from core.data import *
 
 # Create a Database with sqlite3 if there is no database.
-if not os.path.isfile('data/database.db'):
-    create_DB()
-else:
-    print("Database already exists!")
+db = DB("data.db")
+
+
+
+main_menu = """
+    1. Add an item              4. Create an item
+    2. Remove an item           5. Delete an item
+    3. Get an item              6. See all items
+"""
+
+# USER INPUT
+try:
+    while True:
+        ui = input(str(f"$ {socket.gethostname()} "))
+        if ui == "1":
+            print("Add an item")
+        elif ui == "2":
+            pass
+        elif ui == "3":
+            pass
+        elif ui == "4":
+            pass
+        elif ui == "5":
+            pass
+        elif ui == "6":
+            pass
+        else:
+            continue
+
+except KeyboardInterrupt:
+    sys.exit(0)
